295b9acfaa29b1ff60f8358d6a1b8fba1536fa19
Martin Giger <martin@humanoids.be>
2019-01-24 00:17:41 +0100

    fix build

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ cd package
 sha256sum LICENSE > SHA256SUMS
 cd ..
 sha256sum package.json adapter.js > package/SHA256SUMS
-find node_modules -type f -exec sha256sum {} \; >> package/SHA256SUMS
 rm -rf node_modules
 npm ci --production
+find node_modules -type f -exec sha256sum {} \; >> package/SHA256SUMS
 cp -r node_modules ./package
 tar czf ${TARFILE} package
 rm -rf package
