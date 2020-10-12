0bc90fd2c12e0cb47160063a71f8f38a9ef26e9e
Michael Stegeman <michael@stegeman.me>
2020-10-12 12:24:39 -0800

    Fix packaging.

--- package.sh
+++ package.sh
@@ -3,13 +3,13 @@
 TARFILE=`npm pack`
 
 tar xzf ${TARFILE}
-npm ci
+npm install
 npm run licenses
 cd package
 shasum --algorithm 256 manifest.json package.json adapter.js LICENSE README.md > SHA256SUMS
 cd ..
 rm -rf node_modules
-npm ci --production
+npm install --production
 find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> package/SHA256SUMS
 cp -r node_modules ./package
 tar czf ${TARFILE} package
